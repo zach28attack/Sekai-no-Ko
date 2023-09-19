c1e9cdb680c2a71c4f6d4c32edf44519bdd2f9f2
Zachary Casares <rrumble250@gmail.com>
2023-09-19 15:42:22 -0400

    setup translation test form

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,7 +1,32 @@
 import Class from "./HomePage.module.css";
+import {useState} from "react";
 
 function HomePage() {
-  return <div> </div>;
+  const [result, setResult] = useState("");
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    const translation = await fetch(``);
+    // console.log(e.target.value);
+  };
+
+  return (
+    <div>
+      <form>
+        <label>世界</label>
+        <input type="submit" value="世界" onClick={submitHandler} />
+
+        <label>水</label>
+        <input type="submit" value="水" onClick={submitHandler} />
+
+        <label>雪</label>
+        <input type="submit" value="雪" onClick={submitHandler} />
+      </form>
+      <div>
+        <h1>Result</h1>
+        <section>{result}</section>
+      </div>
+    </div>
+  );
 }
 
 export default HomePage;
