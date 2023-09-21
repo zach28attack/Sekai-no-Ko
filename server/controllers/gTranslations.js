778713751c3aba6f5a1a2c00e5ad1b6c18139094
Zachary Casares <rrumble250@gmail.com>
2023-09-21 18:57:31 -0400

    removed redundant console log

--- server/controllers/gTranslations.js
+++ server/controllers/gTranslations.js
@@ -4,8 +4,6 @@ exports.getTranslation = async (req, res) => {
   const google = new Google();
   google.text = req.params.text;
   const translation = await google.translateText();
-  console.log(translation);
-
   if (translation !== undefined) {
     res.status(200).json({message: "Successful Translation", translation: translation});
   }
