425c1f3ddfc47e24142ff57737dc3b97b43ba16b
Zachary Casares <rrumble250@gmail.com>
2023-09-23 18:10:53 -0400

    add japanese REGEX, add kanjiapi for kanji meaning

--- client/JapaneseParser.js
+++ client/JapaneseParser.js
@@ -1,7 +1,25 @@
-export default function JapaneseParser(input) {
-  const TEXT =
-    "霜降り明星 せいやさん 結婚を発表 レゲエ風の曲に乗せ ラジオ番組で「リスナーのみんな聞いてる？ここで発表するって決めていた」 | TBS NEWS DIG - TBS NEWS DIG Powered by JNN";
+// get meaning of each kanji in modal
+const kanjiSearch = async (kanji) => {
+  console.log("kanji:", kanji);
+  const res = await fetch(`https://kanjiapi.dev/v1/kanji/${kanji}`, {
+    method: "GET",
+  });
+  if (res.ok) {
+    const data = await res.json();
+    console.log("kanji:", kanji, "meaning:", data.heisig_en);
+  }
+};
 
-  const parsedString = TEXT.split(" ");
-  console.log(parsedString);
+// underline and add hover modal to vocab words and names
+export default async function JapaneseParser(input) {
+  const jpREGEX = /[\u4e00-\u9faf]/; // regex for kanji by Unicode range
+  const strArr = input.split(" ");
+
+  strArr.forEach((str) => {
+    for (let i = 0; i < str.length; i++) {
+      const isKanji = jpREGEX.test(str[i]);
+      console.log(str[i], isKanji);
+    }
+  });
 }
+// TODO: wrap vocab in span tag with id
