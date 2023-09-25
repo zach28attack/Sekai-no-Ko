c1e7f8483b9c24168db768239115dc396af98418
Zachary Casares <rrumble250@gmail.com>
2023-09-25 16:49:56 -0400

    extract modules

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,28 +3,10 @@ import {useState} from "react";
 import Article from "../components/articles/Article";
 
 function HomePage() {
-  const [result, setResult] = useState("");
-  const [text, setText] = useState("");
   const [articles, setArticles] = useState([]);
   const notAKeyPushedToGithub = "4d251c9cc0fb40a0b202812f573fb7e0"; // a harmless key for newsapi.com so i'm not worrying about protecting it
 
-  const submitHandler = async (e) => {
-    setResult("Pending...");
-    e.preventDefault();
-    try {
-      const res = await fetch(`http://localhost:3000/translate/${text}`, {
-        method: "GET",
-      });
-      if (res.ok) {
-        const data = await res.json();
-        console.log(data);
-        setResult(data.translation);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-  const fetchHandler = async () => {
+  const fetchArticles = async () => {
     const res = await fetch(
       `https://newsapi.org/v2/top-headlines?country=jp&apiKey=${notAKeyPushedToGithub}&pageSize=100`,
       {
@@ -43,20 +25,10 @@ function HomePage() {
 
   return (
     <div className={Class.container}>
-      <form>
-        <div>
-          <input type="text" onChange={inputHandler} />
-          <input type="submit" value="Translate" onClick={submitHandler} />
-        </div>
-      </form>
-      <div>
-        <h1>Result</h1>
-        <section>{result}</section>
-      </div>
-      <button onClick={fetchHandler}>Get Articles</button>
+      <button onClick={fetchArticles}>Get Articles</button>
 
-      {articles.map((data) => (
-        <Article data={data} key={data.url} />
+      {articles.map((data, index) => (
+        <Article key={index} data={data} />
       ))}
     </div>
   );
