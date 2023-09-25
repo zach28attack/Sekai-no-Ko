eb65376da6ff9d54fb5c5b4df1f65f328a29192b
Zachary Casares <rrumble250@gmail.com>
2023-09-25 16:46:48 -0400

    add wrapper to words with kanji

--- client/src/components/articles/kanjiWrapper.jsx
+++ client/src/components/articles/kanjiWrapper.jsx
@@ -1,8 +1,20 @@
+import {useState} from "react";
 import Class from "./KanjiWrapper.module.css";
+import KanjiModal from "./KanjiModal";
 
 // wrap vocab words and names with span element with class and event handler
 function kanjiWrapper(props) {
-  return <span className={Class.kanji}>{props.children}</span>;
+  const [modalActive, setModalActive] = useState(false);
+  const kanjiClickHandler = () => {
+    setModalActive(!modalActive);
+  };
+
+  return (
+    <span className={Class.kanji} onClick={kanjiClickHandler}>
+      {props.children}
+      {modalActive && <KanjiModal kanji={props.children} />}
+    </span>
+  );
 }
 
 export default kanjiWrapper;
