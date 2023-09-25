2a79fc173e780ba4962d50394615ff778ed4c1f2
Zachary Casares <rrumble250@gmail.com>
2023-09-25 16:45:57 -0400

    get meanings and translation for clicked on kanji

---
+++ client/src/components/articles/KanjiModal.jsx
@@ -0,0 +1,46 @@
+import Class from "./KanjiModal.module.css";
+import gTranslate from "../../modules/GoogleTranslate";
+import kanjiSearch from "../../modules/KanjiSearch";
+import {useEffect, useState} from "react";
+
+function KanjiModal({kanji}) {
+  const [meanings, setMeanings] = useState({});
+  const [translation, setTranslation] = useState("");
+
+  const constituants = kanji.split("");
+  const getMeaningsOfConstituants = () => {
+    constituants.forEach(async (kanji) => {
+      const meaning = await kanjiSearch(kanji);
+      setMeanings((prev) => ({...prev, [kanji]: meaning}));
+    });
+  };
+
+  const getTranslation = async () => {
+    const translatedKanji = await gTranslate(kanji);
+    setTranslation(translatedKanji);
+  };
+
+  useEffect(() => {
+    getMeaningsOfConstituants();
+    getTranslation();
+  }, []);
+
+  return (
+    <div className={Class.container}>
+      <div>
+        {kanji} - {translation || "loading..."}
+      </div>
+      <ul>
+        {constituants.map((kanji) => {
+          return (
+            <li>
+              {kanji} - {meanings[kanji] || "Loading..."}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default KanjiModal;
