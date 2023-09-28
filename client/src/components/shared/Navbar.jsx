4d5b06cf103fc3b126dd9663c5fef159162f88f0
Zachary Casares <rrumble250@gmail.com>
2023-09-28 12:55:06 -0400

    reduce navbar to only the Logo

--- client/src/components/shared/Navbar.jsx
+++ client/src/components/shared/Navbar.jsx
@@ -3,13 +3,11 @@ import Class from "./Navbar.module.css";
 export default function Navbar() {
   return (
     <nav className={Class.container}>
-      <ol>
-        <li className={Class.logo}>
-          <ruby>
-            世界<rt>せかい</rt>の<rt></rt>子<rt>こ</rt>
-          </ruby>
-        </li>
-      </ol>
+      <div className={Class.logo}>
+        <ruby>
+          世界<rt>せかい</rt>の<rt></rt>子<rt>こ</rt>
+        </ruby>
+      </div>
     </nav>
   );
 }
